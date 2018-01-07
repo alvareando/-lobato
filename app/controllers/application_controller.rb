dd32ddd156d406cf35108e54b75602946433b0b4
Alvaro Lobato <alobato678@gmail.com>
2018-01-07 16:59:57 +0000

    configuring devise and adding debu gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception # Protect from XSS attacks
   before_action :authenticate_user!
+
+  def configure_permitted_parameters, if: :devise_controller?
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:restaurant_name])
+  end
 end
